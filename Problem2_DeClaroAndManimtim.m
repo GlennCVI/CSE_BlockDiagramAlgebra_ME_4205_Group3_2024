b87a878dfb337108dec7f92f9d178818fb57bca7
Glenn Christian Ilagan <159071273+GlennCVI@users.noreply.github.com>
2024-05-20 22:28:05 +0800

    Update Problem2_DeClaroAndManimtim.m

--- Problem2_DeClaroAndManimtim.m
+++ Problem2_DeClaroAndManimtim.m
@@ -28,44 +28,44 @@ H3_den = [1 -2];
 
 %% Reduce Block Diagrams
 
-H3G4_num = conv(H3_num, G4_num)
-H3G4_den = conv(H3_den, G4_den)
+H3G4_num = conv(H3_num, G4_num);
+H3G4_den = conv(H3_den, G4_den);
 
-H3G4G3_num = conv(H3G4_num, G3_num)
-H3G4G3_den = conv(H3G4_den, G3_den)
+H3G4G3_num = conv(H3G4_num, G3_num);
+H3G4G3_den = conv(H3G4_den, G3_den);
 
-H3G4G3_den_sum = [2 -4 0 1]
+H3G4G3_den_sum = [2 -4 0 1];
 
-TF1_num = conv(H3G4G3_den, G3_num)
-TF1_den = conv(H3G4G3_den_sum, G3_den)
-
-
-
-G2TF1_num = conv(G2_num, TF1_num)
-G2TF1_den = conv(G2_den, TF1_den)
-
-G2TF1_num_G2TF1_den = conv(G2TF1_num, G2TF1_den)
-
-G2TF1_den_H2_den = conv(G2TF1_den, H2_den)
-G2TF1_num_H2_num = conv(G2TF1_num, H2_num)
-G2TF1_den_H2_den_G2TF1_num_H2_num_sum = [2 -4 -2 7 -4 -1 0]
-
-TF2_num = conv(G2TF1_num_G2TF1_den, H2_den)
-TF2_den = conv(G2TF1_den_H2_den_G2TF1_num_H2_num_sum, G2TF1_den)
+TF1_num = conv(H3G4G3_den, G3_num);
+TF1_den = conv(H3G4G3_den_sum, G3_den);
 
 
 
-G1G4_num = conv(G1_num, G4_num)
-G1G4_den = conv(G1_den, G4_den)
+G2TF1_num = conv(G2_num, TF1_num);
+G2TF1_den = conv(G2_den, TF1_den);
 
-G1G4TF2_num = conv(G1G4_num, TF2_num)
-G1G4TF2_den = conv(G1G4_den, TF2_den)
+G2TF1_num_G2TF1_den = conv(G2TF1_num, G2TF1_den);
 
-G1G4TF2_num_G1G4TF2_den = conv(G1G4TF2_num, G1G4TF2_den)
+G2TF1_den_H2_den = conv(G2TF1_den, H2_den);
+G2TF1_num_H2_num = conv(G2TF1_num, H2_num);
+G2TF1_den_H2_den_G2TF1_num_H2_num_sum = [2 -4 -2 7 -4 -1 0];
 
-G1G4TF2_den_H1_den = conv(G1G4TF2_den, H1_den)
-G1G4TF2_num_H1_num = conv(G1G4TF2_num, H1_num)
-G1G4TF2_den_H1_den_G1G4TF2_num_H1_num_sum = [8 -24 -8 72 -32 -56 50 -2 2 16 -20 -2 4 0 0 0]
+TF2_num = conv(G2TF1_num_G2TF1_den, H2_den);
+TF2_den = conv(G2TF1_den_H2_den_G2TF1_num_H2_num_sum, G2TF1_den);
+
+
+
+G1G4_num = conv(G1_num, G4_num);
+G1G4_den = conv(G1_den, G4_den);
+
+G1G4TF2_num = conv(G1G4_num, TF2_num);
+G1G4TF2_den = conv(G1G4_den, TF2_den);
+
+G1G4TF2_num_G1G4TF2_den = conv(G1G4TF2_num, G1G4TF2_den);
+
+G1G4TF2_den_H1_den = conv(G1G4TF2_den, H1_den);
+G1G4TF2_num_H1_num = conv(G1G4TF2_num, H1_num);
+G1G4TF2_den_H1_den_G1G4TF2_num_H1_num_sum = [8 -24 -8 72 -32 -56 50 -2 2 16 -20 -2 4 0 0 0];
 
 TF3_num = conv(G1G4TF2_num_G1G4TF2_den, H1_den)
 TF3_den = conv(G1G4TF2_den_H1_den_G1G4TF2_num_H1_num_sum, G1G4TF2_den)
